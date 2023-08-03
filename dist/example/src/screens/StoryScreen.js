d81a30783c52d754817642835d544db1aad646ec
cicimen <egemengulkilik@gmail.com>
2023-08-03 16:39:37 +0300

    example project RDStoryView implement

--- dist/example/src/screens/StoryScreen.js
+++ dist/example/src/screens/StoryScreen.js
@@ -1,70 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, Switch, Alert, SafeAreaView, } from 'react-native';
-import { RelatedDigital } from '@relateddigital/react-native-huawei';
+import { StyleSheet, ScrollView, SafeAreaView, Button, View, } from 'react-native';
+import { RDStoryView } from '@relateddigital/react-native-huawei';
 export function StoryScreen() {
-    const [isInAppNotificationEnabled, setIsInAppNotificationEnabled] = React.useState(false);
-    const [isPushNotificationEnabled, setIsPushNotificationEnabled] = React.useState(false);
-    const [propertyKey, setPropertyKey] = React.useState('');
-    const [propertyValue, setPropertyValue] = React.useState('');
-    const handleIsInAppNotificationEnabled = (value) => {
-        setIsInAppNotificationEnabled(value);
-        RelatedDigital.setIsInAppNotificationEnabled(value);
-    };
-    const handleIsPushNotificationEnabled = (value) => {
-        setIsPushNotificationEnabled(value);
-        RelatedDigital.setIsPushNotificationEnabled(value, 'yourAppAlias', 'relateddigital-android-test', 'relateddigital-android-huawei-test', true);
-    };
-    const handleSetUserProperty = () => {
-        RelatedDigital.setUserProperty(propertyKey, propertyValue);
-        console.log('User property set.');
-    };
-    const handleRemoveUserProperty = () => {
-        RelatedDigital.removeUserProperty(propertyKey);
-        console.log('User property removed.');
-    };
-    const handleGetToken = () => {
-        RelatedDigital.getToken().then((token) => {
-            console.log(token);
-            Alert.alert('Push Token', token);
-        });
-    };
-    const handleAskForPushNotificationPermission = () => {
-        //RelatedDigital.askForPushNotificationPermission();
-    };
-    const handleSetEmail = () => {
-        RelatedDigital.setEmail('random@gmail.com', true);
-    };
-    const handleGetPushMessages = () => {
-        RelatedDigital.getPushMessages().then((pushMessages) => {
-            console.log(pushMessages);
-        });
-    };
+    function showStory() { }
     return (React.createElement(SafeAreaView, null,
         React.createElement(ScrollView, { contentContainerStyle: styles.container },
-            React.createElement(Text, { style: styles.heading }, "Notifications"),
-            React.createElement(View, { style: styles.switchRow },
-                React.createElement(Text, null, "In-App Notification Enabled:"),
-                React.createElement(Switch, { value: isInAppNotificationEnabled, onValueChange: handleIsInAppNotificationEnabled })),
-            React.createElement(View, { style: styles.switchRow },
-                React.createElement(Text, null, "Push Notification Enabled:"),
-                React.createElement(Switch, { value: isPushNotificationEnabled, onValueChange: handleIsPushNotificationEnabled })),
-            React.createElement(Text, { style: styles.heading }, "User Properties"),
-            React.createElement(Text, null, "Property Key:"),
-            React.createElement(TextInput, { style: styles.input, value: propertyKey, onChangeText: setPropertyKey, placeholder: "Enter property key" }),
-            React.createElement(Text, null, "Property Value:"),
-            React.createElement(TextInput, { style: styles.input, value: propertyValue, onChangeText: setPropertyValue, placeholder: "Enter property value" }),
             React.createElement(View, { style: styles.button },
-                React.createElement(Button, { title: "Set User Property", onPress: handleSetUserProperty })),
-            React.createElement(View, { style: styles.button },
-                React.createElement(Button, { title: "Remove User Property", onPress: handleRemoveUserProperty })),
-            React.createElement(View, { style: styles.button },
-                React.createElement(Button, { title: "Get Token", onPress: handleGetToken })),
-            React.createElement(View, { style: styles.button },
-                React.createElement(Button, { title: "Ask for Push Notification Permission", onPress: handleAskForPushNotificationPermission })),
-            React.createElement(View, { style: styles.button },
-                React.createElement(Button, { title: "Set Email", onPress: handleSetEmail })),
-            React.createElement(View, { style: styles.button },
-                React.createElement(Button, { title: "Get Push Messages", onPress: handleGetPushMessages })))));
+                React.createElement(Button, { title: 'Show Story', onPress: () => showStory() })),
+            React.createElement(RDStoryView, { style: styles.container, onItemClicked: (event) => console.log(event), actionId: '459' }))));
 }
 const styles = StyleSheet.create({
     container: {
--- example/src/screens/StoryScreen.tsx
+++ example/src/screens/StoryScreen.tsx
@@ -1,129 +1,27 @@
 import * as React from 'react';
 import {
   StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
   ScrollView,
-  Switch,
-  Alert,
   SafeAreaView,
+  Button,
+  View,
 } from 'react-native';
-import { RelatedDigital } from '@relateddigital/react-native-huawei';
+import { RDStoryView } from '@relateddigital/react-native-huawei';
 
 export function StoryScreen() {
-  const [isInAppNotificationEnabled, setIsInAppNotificationEnabled] =
-    React.useState(false);
-  const [isPushNotificationEnabled, setIsPushNotificationEnabled] =
-    React.useState(false);
-  const [propertyKey, setPropertyKey] = React.useState('');
-  const [propertyValue, setPropertyValue] = React.useState('');
-
-  const handleIsInAppNotificationEnabled = (value: boolean) => {
-    setIsInAppNotificationEnabled(value);
-    RelatedDigital.setIsInAppNotificationEnabled(value);
-  };
-
-  const handleIsPushNotificationEnabled = (value: boolean) => {
-    setIsPushNotificationEnabled(value);
-    RelatedDigital.setIsPushNotificationEnabled(
-      value,
-      'yourAppAlias',
-      'relateddigital-android-test',
-      'relateddigital-android-huawei-test',
-      true
-    );
-  };
-
-  const handleSetUserProperty = () => {
-    RelatedDigital.setUserProperty(propertyKey, propertyValue);
-    console.log('User property set.');
-  };
-
-  const handleRemoveUserProperty = () => {
-    RelatedDigital.removeUserProperty(propertyKey);
-    console.log('User property removed.');
-  };
-
-  const handleGetToken = () => {
-    RelatedDigital.getToken().then((token) => {
-      console.log(token);
-      Alert.alert('Push Token', token);
-    });
-  };
-
-  const handleAskForPushNotificationPermission = () => {
-    //RelatedDigital.askForPushNotificationPermission();
-  };
-
-  const handleSetEmail = () => {
-    RelatedDigital.setEmail('random@gmail.com', true);
-  };
-
-  const handleGetPushMessages = () => {
-    RelatedDigital.getPushMessages().then((pushMessages) => {
-      console.log(pushMessages);
-    });
-  };
+  function showStory() {}
 
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.heading}>Notifications</Text>
-        <View style={styles.switchRow}>
-          <Text>In-App Notification Enabled:</Text>
-          <Switch
-            value={isInAppNotificationEnabled}
-            onValueChange={handleIsInAppNotificationEnabled}
-          />
+        <View style={styles.button}>
+          <Button title={'Show Story'} onPress={() => showStory()} />
         </View>
-        <View style={styles.switchRow}>
-          <Text>Push Notification Enabled:</Text>
-          <Switch
-            value={isPushNotificationEnabled}
-            onValueChange={handleIsPushNotificationEnabled}
-          />
-        </View>
-        <Text style={styles.heading}>User Properties</Text>
-        <Text>Property Key:</Text>
-        <TextInput
-          style={styles.input}
-          value={propertyKey}
-          onChangeText={setPropertyKey}
-          placeholder="Enter property key"
+        <RDStoryView
+          style={styles.container}
+          onItemClicked={(event) => console.log(event)}
+          actionId={'459'}
         />
-        <Text>Property Value:</Text>
-        <TextInput
-          style={styles.input}
-          value={propertyValue}
-          onChangeText={setPropertyValue}
-          placeholder="Enter property value"
-        />
-        <View style={styles.button}>
-          <Button title="Set User Property" onPress={handleSetUserProperty} />
-        </View>
-        <View style={styles.button}>
-          <Button
-            title="Remove User Property"
-            onPress={handleRemoveUserProperty}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button title="Get Token" onPress={handleGetToken} />
-        </View>
-        <View style={styles.button}>
-          <Button
-            title="Ask for Push Notification Permission"
-            onPress={handleAskForPushNotificationPermission}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button title="Set Email" onPress={handleSetEmail} />
-        </View>
-        <View style={styles.button}>
-          <Button title="Get Push Messages" onPress={handleGetPushMessages} />
-        </View>
       </ScrollView>
     </SafeAreaView>
   );
