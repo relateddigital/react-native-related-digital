fd6d4e38ebcd166281e0b57e1fbe566beff67bf0
bariisarslans <bariisarslans@hotmail.com>
2025-01-22 16:40:58 +0300

    metro config fix

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,75 +1,20 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
 
-// module.exports = {
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: true,
-//       },
-//     }),
-//   },
-// };
-
-// /**
-//  * Metro configuration for React Native
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  */
-
-// module.exports = {
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: false,
-//       },
-//     }),
-//   },
-// };
-
-// ------
-
-// metro.config.js
-//
-// with multiple workarounds for this issue with symlinks:
-// https://github.com/facebook/metro/issues/1
-//
-// with thanks to @johnryan (<https://github.com/johnryan>)
-// for the pointers to multiple workaround solutions here:
-// https://github.com/facebook/metro/issues/1#issuecomment-541642857
-//
-// see also this discussion:
-// https://github.com/brodybits/create-react-native-module/issues/232
-
-const path = require('path')
+const projectRoot = path.resolve(__dirname,'../');
 
 module.exports = {
-  // workaround for an issue with symlinks encountered starting with
-  // metro@0.55 / React Native 0.61
-  // (not needed with React Native 0.60 / metro@0.54)
   resolver: {
     extraNodeModules: new Proxy(
       {},
-      { get: (_, name) => path.resolve('/Users/barisarslan/Desktop/react_native/react-native-related-digital/client', 'node_modules', name) }
-    )
+      {
+        get: (_, name) =>
+          path.resolve(projectRoot, 'client', 'node_modules', name),
+      }
+    ),
   },
 
-  // quick workaround for another issue with symlinks
-  watchFolders: ['/Users/barisarslan/Desktop/react_native/react-native-related-digital/client', '/Users/barisarslan/Desktop/react_native/react-native-related-digital/sdk'],
-
-  // transformer: {
-  //   getTransformOptions: async () => ({
-  //     transform: {
-  //       experimentalImportSupport: false,
-  //       inlineRequires: false,
-  //     },
-  //   }),
-  // },
-}
+  watchFolders: [
+    path.resolve(projectRoot, 'client'),
+    path.resolve(projectRoot, 'sdk'),
+  ],
+};
